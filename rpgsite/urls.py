9d9dec802785aa3b8d9756612fb8ffd31da1c238
cantarino10 <vinicius.cantarino10@gmail.com>
2025-07-24 11:56:32 +0100

    Update urls.py

--- rpgsite/urls.py
+++ rpgsite/urls.py
@@ -1,33 +1,33 @@
-"""
-URL configuration for rpgsite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from main.sitemaps import (
+    StaticViewSitemap,
+    ClasseSitemap,
+    SpellSitemap,
+    FeatSitemap,
+    EnhancementSitemap,
+    ItemSitemap,
+    RaceSitemap,
+    HandbookSitemap,
+)
 
 sitemaps = {
     'static': StaticViewSitemap,
+    'classes': ClasseSitemap,
+    'spells': SpellSitemap,
+    'feats': FeatSitemap,
+    'enhancements': EnhancementSitemap,
+    'items': ItemSitemap,
+    'races': RaceSitemap,
+    'handbooks': HandbookSitemap,
 }
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('users/', include('users.urls')),
-    path('tools/',include('tools.urls')),
+    path('tools/', include('tools.urls')),
     path('captcha/', include('captcha.urls')),
+
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    
-    
 ]
