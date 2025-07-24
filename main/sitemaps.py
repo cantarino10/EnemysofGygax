0fc3be9be8bcdcf8cf2c3d6b25f2d30c5f8ba0b1
cantarino10 <vinicius.cantarino10@gmail.com>
2025-07-24 11:54:20 +0100

    Update sitemaps.py

--- main/sitemaps.py
+++ main/sitemaps.py
@@ -1,26 +1,28 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-from .models import Handbook, Classe, Spell, Feat  # ou os nomes corretos
 
 class StaticViewSitemap(Sitemap):
-    priority = 0.8
+    priority = 0.5
     changefreq = 'weekly'
 
     def items(self):
-        return ['index', 'classes', 'races', 'spells', 'items', 'enhancements', 'handbooks']
+        return [
+            'index',
+            'handbooks',
+            'races',
+            'classes',
+            'items',
+            'feats',
+            'spells',
+            'enhancements',
+            # URLs que não precisam de parâmetro
+            'roll_dice',
+            'stats_builder',
+            'encounter',
+            'login',
+            'register',
+            'password_reset',
+        ]
 
     def location(self, item):
         return reverse(item)
-
-class HandbookSitemap(Sitemap):
-    changefreq = "monthly"
-    priority = 0.6
-
-    def items(self):
-        return Handbook.objects.all()
-
-    def location(self, obj):
-        return reverse('handbook', args=[obj.name])
-
-# Repita para outras classes se quiser:
-# ClasseSitemap, SpellSitemap, FeatSitemap, etc.
