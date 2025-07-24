ec0a8c9194fe840bebc5c1d4c07c38676fb9cbeb
cantarino10 <vinicius.cantarino10@gmail.com>
2025-07-24 11:33:48 +0100

    Update views.py

--- main/views.py
+++ main/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.decorators import login_required      #metodo para rest
 #decorator são para alterar o comportamento da função sem mudar a função
 from django.contrib.auth.decorators import user_passes_test
 from django.core.paginator import Paginator
-
+from django.http import HttpResponse
 import pandas as pd
 from django.shortcuts import get_object_or_404
 import re,json,requests
